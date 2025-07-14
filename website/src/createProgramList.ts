6fbe0cb66903ba4feb9a4bae23e996bc5c6c339b
Niklas <niklas.mikeler@gmail.com>
2025-07-14 05:24:34 +0200

    Fix handelbars helper

--- website/src/createProgramList.ts
+++ website/src/createProgramList.ts
@@ -10,6 +10,9 @@ Handlebars.registerHelper('isString', function (arg, options) {
 Handlebars.registerHelper('isArray', function (arg, options) {
     return Array.isArray(arg) ? options.fn(this) : options.inverse(this)
 })
+Handlebars.registerHelper('replaceWinget', function (text, search, replace) {
+  return text.split(search).join(replace);
+});
 
 export const createProgramList = (programs: Program[]) => {
     const div = document.createElement("div")
